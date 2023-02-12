ac2c9c645c3c20108093af259d57f358f75df7ff
nomisum <nomisum@gmail.com>
2023-02-12 15:27:22 +0100

    added more sounds to usersounds

--- USER/userSounds.hpp
+++ USER/userSounds.hpp
@@ -32,7 +32,12 @@ class evac_call_normal
 };
 
 
+
+
+
 // NADIA
+
+
 class nadia_pee
 {
 	name	= "nadia_pee";
@@ -40,12 +45,14 @@ class nadia_pee
 	titles[] = {};
 };
 
-class nadia_peeBefore_1_fear
+
+// CONFUSED
+class nadia_confused_1_fear
 {
-	name	= "nadia_peeBefore_1_fear";
-	sound[]	= {"\USER\sounds\nadia_peeBefore_1_fear.ogg", db + 10, 1.0 };
+	name	= "nadia_confused_1_fear";
+	sound[]	= {"\USER\sounds\nadia_confused_1_fear.ogg", db + 10, 1.0 };
 	titles[] = {
-			1, "<t color='#ff0000' shadow='2'>I need to pee</t>"
+			0, "<t color='#ffffff' shadow='2'>What are you doing</t>"
 		};
 
 	forceTitles = true;            // will display titles even if global show titles option is off
@@ -54,12 +61,201 @@ class nadia_peeBefore_1_fear
 	titlesStructured = true;    // treat titles as Structured Text 
 };
 
+class nadia_confused_2_fear
+{
+	name	= "nadia_confused_2_fear";
+	sound[]	= {"\USER\sounds\nadia_confused_2_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>This is a nightmare</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_dontknow_1_calm
+{
+	name	= "nadia_dontknow_1_calm";
+	sound[]	= {"\USER\sounds\nadia_dontknow_1_calm.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>This is a nightmare</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_dontknow_1_fear
+{
+	name	= "nadia_dontknow_1_fear";
+	sound[]	= {"\USER\sounds\nadia_dontknow_1_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>I dont know</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_dontknow_2_fear
+{
+	name	= "nadia_dontknow_2_fear";
+	sound[]	= {"\USER\sounds\nadia_dontknow_2_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>I dont know</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+
+
+// GREET
+class nadia_greet_1_calm
+{
+	name	= "nadia_greet_1_calm";
+	sound[]	= {"\USER\sounds\nadia_greet_1_calm.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>Hey boys</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_greet_1_fear
+{
+	name	= "nadia_greet_1_fear";
+	sound[]	= {"\USER\sounds\nadia_greet_1_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>Hello</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_greet_2_fear
+{
+	name	= "nadia_greet_2_fear";
+	sound[]	= {"\USER\sounds\nadia_greet_2_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>Hey boys</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_greet_3_fear
+{
+	name	= "nadia_greet_3_fear";
+	sound[]	= {"\USER\sounds\nadia_greet_3_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>Hello</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+
+
+// NO
+class nadia_no_1_calm
+{
+	name	= "nadia_no_1_calm";
+	sound[]	= {"\USER\sounds\nadia_no_1_calm.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>No</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_no_1_fear
+{
+	name	= "nadia_no_1_fear";
+	sound[]	= {"\USER\sounds\nadia_no_1_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>No</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_no_2_fear
+{
+	name	= "nadia_no_2_fear";
+	sound[]	= {"\USER\sounds\nadia_no_2_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>No</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_no_3_fear
+{
+	name	= "nadia_no_3_fear";
+	sound[]	= {"\USER\sounds\nadia_no_3_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>No</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_no_4_fear
+{
+	name	= "nadia_no_4_fear";
+	sound[]	= {"\USER\sounds\nadia_no_4_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			0, "<t color='#ffffff' shadow='2'>No</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+// PEE
 class nadia_peeAfter_1_calm
 {
 	name	= "nadia_peeAfter_1_calm";
 	sound[]	= {"\USER\sounds\nadia_peeAfter_1_calm.ogg", db + 10, 1.0 };
 	titles[] = {
-			1, "<t color='#ff0000' shadow='2'>Ah, that was necessary</t>"
+			1, "<t color='#ffffff' shadow='2'>Ah, that was necessary</t>"
 		};
 
 	forceTitles = true;            // will display titles even if global show titles option is off
@@ -67,6 +263,293 @@ class nadia_peeAfter_1_calm
 	titlesSize = 0.1;            // titles font size
 	titlesStructured = true;    // treat titles as Structured Text 
 };
+
+class nadia_peeBefore_1_fear
+{
+	name	= "nadia_peeBefore_1_fear";
+	sound[]	= {"\USER\sounds\nadia_peeBefore_1_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>I need to pee</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_peeBefore_2_fear
+{
+	name	= "nadia_peeBefore_2_fear";
+	sound[]	= {"\USER\sounds\nadia_peeBefore_2_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Please, can I pee</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_peeBefore_3_fear
+{
+	name	= "nadia_peeBefore_3_fear";
+	sound[]	= {"\USER\sounds\nadia_peeBefore_3_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>I really need to pee now</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+
+class nadia_quasimdead_1_calm
+{
+	name	= "nadia_quasimdead_1_calm";
+	sound[]	= {"\USER\sounds\nadia_quasimdead_1_calm.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Good job boys, now my main source of income is dead</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_quasimdead_1_fear
+{
+	name	= "nadia_quasimdead_1_fear";
+	sound[]	= {"\USER\sounds\nadia_quasimdead_1_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Good job boys, now my main source of income is dead</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_quasimdead_2_calm
+{
+	name	= "nadia_quasimdead_2_calm";
+	sound[]	= {"\USER\sounds\nadia_quasimdead_2_calm.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>This is a fucking nightmare</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+
+class nadia_quasimlove_1_seduce
+{
+	name	= "nadia_quasimlove_1_seduce";
+	sound[]	= {"\USER\sounds\nadia_quasimlove_1_seduce.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Quasim, my love</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_quasimlove_2_seduce
+{
+	name	= "nadia_quasimlove_2_seduce";
+	sound[]	= {"\USER\sounds\nadia_quasimlove_2_seduce.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>O boy</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+
+class nadia_quasimlove_3_seduce
+{
+	name	= "nadia_quasimlove_3_seduce";
+	sound[]	= {"\USER\sounds\nadia_quasimlove_3_seduce.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Uuuh</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+
+class nadia_quasimlove_4_seduce
+{
+	name	= "nadia_quasimlove_4_seduce";
+	sound[]	= {"\USER\sounds\nadia_quasimlove_4_seduce.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Give it to me Quasim</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+
+class nadia_quasimlove_5_seduce
+{
+	name	= "nadia_quasimlove_5_seduce";
+	sound[]	= {"\USER\sounds\nadia_quasimlove_5_seduce.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Oh babe..</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+
+class nadia_whatshappening_1_fear
+{
+	name	= "nadia_whatshappening_1_fear";
+	sound[]	= {"\USER\sounds\nadia_whatshappening_1_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Whats happening?</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_whatshappening_2_fear
+{
+	name	= "nadia_whatshappening_2_fear";
+	sound[]	= {"\USER\sounds\nadia_whatshappening_2_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Whats happening?</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_yes_1_calm
+{
+	name	= "nadia_yes_1_calm";
+	sound[]	= {"\USER\sounds\nadia_yes_1_calm.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Yes</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_yes_1_calm
+{
+	name	= "nadia_yes_1_calm";
+	sound[]	= {"\USER\sounds\nadia_yes_1_calm.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Yes</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_yes_1_fear
+{
+	name	= "nadia_yes_1_fear";
+	sound[]	= {"\USER\sounds\nadia_yes_1_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Yes</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_yes_2_fear
+{
+	name	= "nadia_yes_2_fear";
+	sound[]	= {"\USER\sounds\nadia_yes_2_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Yes</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_yes_3_fear
+{
+	name	= "nadia_yes_3_fear";
+	sound[]	= {"\USER\sounds\nadia_yes_3_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Yes</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_yes_4_fear
+{
+	name	= "nadia_yes_4_fear";
+	sound[]	= {"\USER\sounds\nadia_yes_4_fear.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Yes</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
+class nadia_yes_1_seduce
+{
+	name	= "nadia_yes_1_seduce";
+	sound[]	= {"\USER\sounds\nadia_yes_1_seduce.ogg", db + 10, 1.0 };
+	titles[] = {
+			1, "<t color='#ffffff' shadow='2'>Yes</t>"
+		};
+
+	forceTitles = true;            // will display titles even if global show titles option is off
+	titlesFont = "RobotoCondensed";        // titles font family
+	titlesSize = 0.1;            // titles font size
+	titlesStructured = true;    // treat titles as Structured Text 
+};
+
 
 
 // KILLERKEBAP
